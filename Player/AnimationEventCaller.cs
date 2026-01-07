768f63cad508bd233345ee813859472cdfef096a
MDiaz581 <madiaz581@gmail.com>
2026-01-06 19:15:08 -0500

    Cleaned up files

--- Player/AnimationEventCaller.cs
+++ Player/AnimationEventCaller.cs
@@ -4,21 +4,6 @@ using UnityEngine;
 
 public class AnimationEventCaller : MonoBehaviour
 {
-
-    public PlayerBehaviorRootmotion PBR;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void Jump()
     {
         PBR.grounded = false;
